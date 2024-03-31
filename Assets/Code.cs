61d4febbd98393d749dab4389257a886383dfb96
Seydlitz-code <153933205+Seydlitz-code@users.noreply.github.com>
2024-04-01 02:10:35 +0900

    Initial commit

---
+++ Assets/Code.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Code : MonoBehaviour
+{
+    private void Awake()
+    {
+        Stack stack = new Stack();
+        for(int i = 1; i < 5; ++ i)
+        {
+            stack.Push(i);
+        }
+        Debug.Log($"Stack Count : {stack.Count}");
+		Debug.Log($"마지막에 추가된 요소 : {stack.Peek()}");
+		Debug.Log($"Stack Count : {stack.Count}");
+        object data = stack.Pop();
+		Debug.Log($"스텍에서 빠져나온 데이터 : {data}");
+		Debug.Log($"Stack Count : {stack.Count}");
+        stack.Clear();
+		Debug.Log($"Stack Count : {stack.Count}");
+	}
+}
